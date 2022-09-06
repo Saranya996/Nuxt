1f0742d3abe1da0c69e96ff70e56a8b4bb8cc0b6
SaranyaPulenthirarasa <91838715+Saranya996@users.noreply.github.com>
2022-09-06 16:29:57 +0530

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div class="p-4 bg-gray-600 text-white">
+        <NuxtLink to='/homepage' class="p-2 bg-red-600 text-white rounded">Home</NuxtLink>
+        <NuxtLink to='/login' class="p-2 bg-red-600 text-white rounded">Login</NuxtLink>
+        </div>
+        <Nuxt />
+    </div>
+</template>
+<script>
+    export default{
+        
+    }
+</script>
